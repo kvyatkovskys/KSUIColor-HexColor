1c7ed8744e94173dab5818d446550b6c82a308d2
Sergei <sergejkvyatkovskij@gmail.com>
2016-09-03 10:44:33 +0300

    Add files via upload

---
+++ KSUIColor+Hexadecimal.h
@@ -0,0 +1,13 @@
+//
+//  Created by Пользователь on 22.08.16.
+//  Copyright © 2016 Kvyatkovskys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Hexadecimal)
+
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+
+@end
---
+++ KSUIColor+Hexadecimal.m
@@ -0,0 +1,18 @@
+//  Created by Пользователь on 22.08.16.
+//  Copyright © 2016 Kvyatkovskys. All rights reserved.
+//
+
+#import "KSUIColor+Hexadecimal.h"
+
+@implementation UIColor (Hexadecimal)
+
++ (UIColor *)colorWithHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
+
+@end
